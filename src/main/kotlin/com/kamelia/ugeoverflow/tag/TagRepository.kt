a667144129db58ed442abc17c931cd21dd0137eb
ZwenDo <lorris.creantor@gmail.com>
2023-03-07 09:57:24 +0100

    Added TagRepository

---
+++ src/main/kotlin/com/kamelia/ugeoverflow/tag/TagRepository.kt
@@ -0,0 +1,8 @@
+package com.kamelia.ugeoverflow.tag
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+
+@Repository
+interface TagRepository : JpaRepository<Tag, String>
