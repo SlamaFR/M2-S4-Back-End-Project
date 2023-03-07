795311ab9bc9b96203d19005632ac2b1ed33a7be
notKamui <teillard.jimmy@outlook.fr>
2023-03-07 09:52:56 +0100

    Use jackson kt module

--- src/main/kotlin/com/kamelia/ugeoverflow/UGEOverflowApplication.kt
+++ src/main/kotlin/com/kamelia/ugeoverflow/UGEOverflowApplication.kt
@@ -1,11 +1,26 @@
 package com.kamelia.ugeoverflow
 
+import com.fasterxml.jackson.module.kotlin.kotlinModule
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
-class BackendProjectApplication
+class BackendProjectApplication {
+
+    @Bean
+    fun objectMapperBuilder(): Jackson2ObjectMapperBuilder = Jackson2ObjectMapperBuilder()
+        .modulesToInstall(kotlinModule())
+        .also { LOGGER.info("Loaded Kotlin Jackson module") }
+
+    companion object {
+
+        private val LOGGER = LoggerFactory.getLogger(this::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<BackendProjectApplication>(*args)
