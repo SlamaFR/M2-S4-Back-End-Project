dfb37ab2590349a677bd75f2f530c0ed89f570cc
ZwenDo <lorris.creantor@gmail.com>
2023-03-07 11:03:45 +0100

    Added user and trust evaluation

---
+++ src/main/kotlin/com/kamelia/ugeoverflow/evaluation/TrustEvaluation.kt
@@ -0,0 +1,33 @@
+package com.kamelia.ugeoverflow.evaluation
+
+import com.kamelia.ugeoverflow.AbstractIdEntity
+import com.kamelia.ugeoverflow.user.User
+import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "trust_evaluation")
+class TrustEvaluation(
+    @ManyToOne
+    var evaluated: User,
+    trust: Int,
+) : AbstractIdEntity() {
+
+    init {
+        checkTrust(trust)
+    }
+
+    var trust: Int = trust
+        set(value) {
+            checkTrust(value)
+            field = value
+        }
+
+    private fun checkTrust(trust: Int) {
+        require(trust in -50..50) {
+            "Invalid evaluation: $trust. Evaluation must be in range [-50, 50]."
+        }
+    }
+
+}
---
+++ src/main/kotlin/com/kamelia/ugeoverflow/user/User.kt
@@ -0,0 +1,54 @@
+package com.kamelia.ugeoverflow.user
+
+import com.kamelia.ugeoverflow.AbstractIdEntity
+import com.kamelia.ugeoverflow.evaluation.TrustEvaluation
+import jakarta.persistence.Entity
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotBlank
+
+@Entity
+@Table(name = "user")
+class User(
+    username: String,
+    password: String,
+    following: Set<User>,
+    trustEvaluations: Set<TrustEvaluation>,
+) : AbstractIdEntity() {
+
+    @ManyToMany
+    private var _following: MutableSet<User> = following.toMutableSet()
+
+    @OneToMany
+    private var _trustEvaluations: MutableSet<TrustEvaluation> = trustEvaluations.toMutableSet()
+
+    @NotBlank
+    var username: String = username
+        set(value) {
+            require(value.isNotBlank()) { "Username cannot be blank" }
+            field = value
+        }
+
+    @NotBlank
+    var password: String = password
+        set(value) {
+            require(value.isNotBlank()) { "Password cannot be blank" }
+            field = value
+        }
+
+    var following: Set<User> = _following
+        get() = _following
+        set(value) {
+            field = value
+            _following = value.toMutableSet()
+        }
+
+    var trustEvaluations: Set<TrustEvaluation> = _trustEvaluations
+        get() = _trustEvaluations
+        set(value) {
+            field = value
+            _trustEvaluations = value.toMutableSet()
+        }
+
+}
