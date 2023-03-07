abf447ccb533614bc947a84129b8a67ffbfc2bcd
ZwenDo <lorris.creantor@gmail.com>
2023-03-07 16:05:48 +0100

    Added Post page request in controller

---
+++ src/main/kotlin/com/kamelia/ugeoverflow/post/PostRepository.kt
@@ -0,0 +1,10 @@
+package com.kamelia.ugeoverflow.post
+
+import java.util.*
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostRepository : JpaRepository<Post, UUID> {
+
+}
