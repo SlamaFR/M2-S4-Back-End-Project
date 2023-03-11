176ff15be79836937b4117b1442939bcd9b9ee2f
notKamui <teillard.jimmy@outlook.fr>
2023-03-11 21:44:42 +0100

    Class rename

--- src/main/kotlin/com/kamelia/ugeoverflow/session/AuthMVController.kt
+++ src/main/kotlin/com/kamelia/ugeoverflow/session/AuthMVController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/auth")
-class AuthMVCController {
+class AuthMVController {
 
     @GetMapping
     fun auth(
