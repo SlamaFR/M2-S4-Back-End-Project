2b137e2893d426f8749cd09ae03848e476a04187
notKamui <teillard.jimmy@outlook.fr>
2023-03-13 23:04:46 +0100

    Serve static css

---
+++ src/main/kotlin/com/kamelia/ugeoverflow/core/MVCConfig.kt
@@ -0,0 +1,15 @@
+package com.kamelia.ugeoverflow.core
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class MVCConfig : WebMvcConfigurer {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/static/css/**").addResourceLocations("classpath:/static/css/")
+    }
+}
