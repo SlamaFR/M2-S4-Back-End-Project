edc926c3681c4e24a1b1c852ce4e08dd8ea8c291
notKamui <teillard.jimmy@outlook.fr>
2023-03-17 20:59:04 +0100

    Added interceptor

--- src/main/kotlin/com/kamelia/ugeoverflow/core/MVCConfig.kt
+++ src/main/kotlin/com/kamelia/ugeoverflow/core/MVCConfig.kt
@@ -2,6 +2,7 @@ package com.kamelia.ugeoverflow.core
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -11,5 +12,9 @@ class MVCConfig : WebMvcConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/static/css/**").addResourceLocations("classpath:/static/css/")
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(UserInterceptor())
     }
 }
