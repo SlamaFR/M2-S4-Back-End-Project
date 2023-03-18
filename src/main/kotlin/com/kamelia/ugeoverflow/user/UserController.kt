76b03391b8f191c3ac81fe0cfd7766df4d52a372
notKamui <teillard.jimmy@outlook.fr>
2023-03-18 20:21:59 +0100

    User can update trust of followed user

--- src/main/kotlin/com/kamelia/ugeoverflow/user/UserController.kt
+++ src/main/kotlin/com/kamelia/ugeoverflow/user/UserController.kt
@@ -6,11 +6,14 @@ import com.kamelia.ugeoverflow.utils.Roles
 import com.kamelia.ugeoverflow.utils.currentUser
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import org.springframework.security.access.annotation.Secured
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -72,20 +75,13 @@ class UserController(
         @Valid @ModelAttribute("updateTrustForm") updateTrustForm: UpdateTrustForm,
         model: Model,
     ): String {
-        val user = currentUser().let { userService.findByIdOrNull(it.id) }
-        if (user == null) {
-            model.addAttribute("errorMessage", "User not found")
-            return "error/404"
-        }
-
-        val followed = user.followed.firstOrNull { it.followed.id == id }
+        val followed = followingService.getFollowedUsers().firstOrNull { it.id == id }
         if (followed == null) {
             model.addAttribute("errorMessage", "You do not follow this user")
             return "error/404"
         }
 
-        model.addAttribute("user", user)
-        model.addAttribute("evaluationModel", followed)
+        model.addAttribute("followed", followed)
         updateTrustForm.trust = followed.trust
 
         return "user/evaluate"
@@ -99,23 +95,18 @@ class UserController(
         model: Model,
         binding: BindingResult,
     ): String {
-        if (binding.hasErrors()) return "redirect:/user/details/$id?updateSuccess=false"
+        val trust = updateTrustForm.trust
+        if (binding.hasErrors() || trust == null || trust < 1 || trust > 20) return "redirect:/user/details/$id?updateSuccess=false"
 
-        val user = currentUser().let { userService.findByIdOrNull(it.id) }
-        if (user == null) {
-            model.addAttribute("errorMessage", "User not found")
-            return "error/404"
-        }
-
-        val followed = user.followed.firstOrNull { it.followed.id == id }
+        val followed = followingService.getFollowedUsers().firstOrNull { it.id == id }
         if (followed == null) {
             model.addAttribute("errorMessage", "You do not follow this user")
             return "error/404"
         }
 
-        // TODO: Update trust
+        followingService.evaluateFollowed(followed.id, trust)
 
-        return "redirect:/user/details/$id?updateSuccess=true"
+        return "redirect:/user/details?updateSuccess=true"
     }
 
     @Secured(Roles.USER)
@@ -138,12 +129,16 @@ class UserController(
     }
 }
 
+@Validated
 class UpdatePasswordForm(
     var oldPassword: String = "",
     var newPassword: String = "",
     var confirmPassword: String = "",
 )
 
+@Validated
 class UpdateTrustForm(
+    @Min(1)
+    @Max(20)
     var trust: Int? = null,
 )
