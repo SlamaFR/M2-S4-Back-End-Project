d2245dbca5f298f2ce44906e6e56b2d310f9354c
ZwenDo <lorris.creantor@gmail.com>
2023-03-19 01:45:40 +0100

    Fixed the project. Yeah, I know 😎.

--- src/main/kotlin/com/kamelia/ugeoverflow/session/SessionRefreshFilter.kt
+++ src/main/kotlin/com/kamelia/ugeoverflow/session/SessionRefreshFilter.kt
@@ -3,7 +3,11 @@ package com.kamelia.ugeoverflow.session
 import com.kamelia.ugeoverflow.core.InvalidRequestException
 import com.kamelia.ugeoverflow.core.toUUIDFromBase64OrNull
 import com.kamelia.ugeoverflow.core.toUUIDOrNull
-import com.kamelia.ugeoverflow.utils.*
+import com.kamelia.ugeoverflow.utils.Cookies
+import com.kamelia.ugeoverflow.utils.Roles
+import com.kamelia.ugeoverflow.utils.Routes
+import com.kamelia.ugeoverflow.utils.getCookieOrNull
+import com.kamelia.ugeoverflow.utils.removeCookie
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -38,6 +42,7 @@ class SessionRefreshFilter(
                 response.removeCookie(Cookies.REFRESH_TOKEN, Routes.Auth.REFRESH)
                 response.sendRedirect("/auth?error=Session+expired:+Please+log+in")
             }
+            return
         }
 
         filterChain.doFilter(request, response)
